d65bebd5b18f04ccb0deaa8e1228fc07463176f5
Heidi Elmi <Heidi.Elmi@frontiersoftware.com.au>
2021-01-28 00:27:54 +1100

    changes to the services and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "" , component: LandingPageComponent},
+  { path: "vendingmachine", loadChildren: () => import("./vending-machine/vending-machine.module")
+                                                .then(m => m.VendingMachineModule)}, 
+   { path: "**" , redirectTo: ""}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NgxsModule } from '@ngxs/store';
+import { AppState } from "./state/app.state";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, BrowserAnimationsModule, MaterialModule,
+    NgxsModule.forRoot([AppState]),
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
